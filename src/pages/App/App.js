9c9d989c7971280a901e23e3b2f4c1d5387ec763
Nate Lynch <natelynch@Nates-MBP.lan>
2020-10-12 11:32:18 -0400

    get react-router working

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,12 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route, Link, NavLink, Switch } from "react-router-dom";
 import './App.css';
+import LoginPage from '../LoginPage/LoginPage'
+import SignupPage from '../SignupPage/SignupPage'
 
-function App() {
-  return (
-    <div>
-      Hello
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <nav>
+            <NavLink exact to="/login">
+              LOG IN
+            </NavLink>
+            &nbsp;
+            <NavLink exact to="/signup">
+              SIGN UP
+            </NavLink>
+          </nav>
+        </header>
+        <main>
+          <Switch>
+            <Route 
+              exact 
+              path="/login" 
+              render={({ history }) => (
+                <LoginPage 
+                  history={history}
+                />
+              )}
+            />
+          </Switch>
+          <Switch>
+            <Route 
+              exact 
+              path="/signup" 
+              render={({ history }) => (
+                <SignupPage 
+                  history={history}
+                />
+              )}
+            />
+          </Switch>
+          Hello
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class LoginPage extends Component {
+    render() {
+        return (
+            <>
+                Login
+            </>
+        )
+    }
+}
+
+export default LoginPage;
